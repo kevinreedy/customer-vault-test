47c12e06f79d1e4c066ef653bb9768d9f0fe43f2
Kevin Reedy <kevinreedy@gmail.com>
2016-01-12 11:35:10 -0800

    stub out vault item with data bag for testing

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures vault-test'
 long_description 'Installs/Configures vault-test'
 version '0.1.0'
+
+depends 'chef-vault'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'chef-vault'
+
+ldap_authtok = chef_vault_item("passwords", "ldap_default")
+#puts ldap_authtok.inspect
+template '/tmp/sssd.conf' do
+  source 'sssd.conf.erb'
+  mode '0600'
+  owner 'root'
+  group 'root'
+  variables({
+    :ldap_default_authtok => ldap_authtok['ldap_default'],
+  })
+end
